e4bfca1424d40d80ecfa6baaffb3ee9582304cfb
steve whitehouse <stewhites@blueyonder.co.uk>
2024-01-25 15:47:23 +0000

    add api key check functionality

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const API_KEY = "kTi9jZOTCxTQ7muNWyr7E1sL2bo"
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click",e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString =`${API_URL}?api_key=${API_KEY}`;
+        const response =await fetch(queryString);
+        const data  = await response.json();
+        if (response.ok) {displayStatus(data);}
+        else {
+            throw new Error(data.error);
+        }
+}
+
+function displayStatus(data){
+    let heading = "API Key Status:";
+    let results = `<div>Your Key is valid until:</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
